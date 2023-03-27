29ec33752a44f5c04895672407689dddddc4119a
Александр <hyperpositron@gmail.com>
2023-03-27 13:14:14 +0200

    Создаем класс Validator.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Validator validator = new Validator();
+        boolean result = Validator.validate("teSt_1234", "D_43tfre43_9", "D_94jfgt432d_8");
+        if (result) {
+            System.out.println("Логин и пароль введен правельно");
+        } else {
+            System.out.println("Логин введен правильно");
+        }
     }
+}
 }
---
+++ src/Validator.java
@@ -0,0 +1,12 @@
+public class Validator {
+
+    private Validator(){
+
+    }
+    public static boolean validate (String login, String password, String confirmPassword){
+        if (Objects.isNull(login) || login.length()>20 ){
+
+        }
+
+    }
+}
