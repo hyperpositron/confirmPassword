d76095ce901dc8c922324141ee8173493b73f43b
Александр <hyperpositron@gmail.com>
2023-03-27 15:02:48 +0200

    Создаем Интерфейс Checker Создаем Класс LoopChecker Прикручиваем полиморфизм через Интерфейс

---
+++ src/LoopChecker.java
@@ -0,0 +1,17 @@
+public class LoopChecker implements Checker {
+    private static final String ALLOWED_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+
+
+    @Override
+    public boolean void check(String s) {
+        char[] chars = s.toCharArray();
+        for (char symbol : chars) {
+            if (!ALLOWED_CHARACTERS.contains(Character.toString(chars[symbol]))) {
+                return false;
+
+            }
+        }
+        return true;
+
+    }
+}
